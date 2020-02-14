51929ac2283b9dbd30a9e6d111c18b346c5664ae
Odin <odin@severinsen.no>
2020-02-14 14:31:47 +0100

    Rename modules and timer ex in main.go

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"time"
+	"fmt"
+)
+
+func timerCallback(timer *time.Timer, x int, wait chan int) {
+	<-(*timer).C
+	fmt.Printf("Got %d after 1 seconds\n", x)
+	wait <- 0
+}
+
+func callback2(x int, wait chan int) {
+	fmt.Printf("Callback %d\n", x)
+	wait <- 0
+}
 
 func main() {
-
+	wait := make(chan int)
+	x := 1
+	time.AfterFunc(1 * time.Second, func () {
+		callback2(x, wait)
+	})
+	// timer := time.NewTimer(1 * time.Second)
+	// go timerCallback(timer, 20, wait)
+	<-wait
 }
