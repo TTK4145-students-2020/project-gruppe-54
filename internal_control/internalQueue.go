b5953f148a452ad1a07203a41db8abb373f58fb1
Eirik Daltveit <eirikjda@stud.ntnu.no>
2020-02-21 13:44:05 +0100

    fixed some shit in internal

---
+++ internal_control/internalQueue.go
@@ -0,0 +1,81 @@
+package driver
+#import "./elevio"
+
+numFloors = 4
+var internalQueue [3][numFloors]int
+
+type Button struct {
+	Dir   MotorDirection
+	Floor int
+}
+
+
+func ChooseDirection() Dir{
+	switch elevio.MotorDirection {
+	case elevio.MD_Stop:
+		if ordersAbove(elevator) {
+			return elevio.MD_Up
+		} else if ordersBelow(elevator) {
+			return elevio.MD_Down
+		} else {
+			return elevio.MD_Stop
+		}
+	case elevio.MD_Up:
+		if ordersAbove(elevator) {
+			return elevio.MD_Up
+		} else if ordersBelow(elevator) {
+			return elevio.MD_Down
+		} else {
+			return elevio.MD_Stop
+		}
+
+	case elevio.MD_Down:
+		if ordersBelow(elevator) {
+			return elevio.MD_Down
+		} else if ordersAbove(elevator) {
+			return elevio.MD_Up
+		} else {
+			return elevio.MD_Stop
+		}
+	}
+	return elevio.MD_Stop
+}
+
+func addOrder(Floor int, typeOfOrder int) {
+	internalQueue[typeOfOrder][Floor] = 1
+}
+
+func deleteOrder(currentFloor int) {
+	for i := 0; i < 3; i++ {
+		internalQueue[i][currentFloor] = 0
+	}
+}
+
+func ordersInFloor(currentFloor int ) int{
+	for i := 0; i < 3; i++ {
+		if internalQueue[i][currentFloor] == 1 {
+			return 1
+		}
+	}
+	return 0
+}
+
+
+func ordersAbove(currentFloor int) int {
+	for i := currentFloor + 1; i < numFloors-1; i++ {
+		if internalQueue[0][i] == 1 || internalQueue[1][i] == 1 || internalQueue[2][i] == 1 {
+			return 1
+		}
+	}
+	return 0
+}
+
+func ordersBelow(currentFloor int) int {
+	for i := currentFloor - 1; i > -1; i-- {
+		if internalQueue[0][i] == 1 || internalQueue[1][i] == 1 || internalQueue[2][i] == 1 {
+			return 1
+		}
+	}
+	return 0
+}
+
