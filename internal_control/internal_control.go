298cd6c7327704bb98b5db43f7c321d8507b3dec
Eirik Daltveit <eirikjda@stud.ntnu.no>
2020-02-21 15:22:35 +0100

    FSmdfaf

--- internal_control/internal_control.go
+++ internal_control/internal_control.go
@@ -16,7 +16,8 @@ func main(){
     
     drv_buttons := make(chan elevio.ButtonEvent)
     drv_floors  := make(chan int)
-    drv_stop    := make(chan bool)    
+	drv_stop    := make(chan bool)    
+	orderDone   := make(chan bool)
     
     go elevio.PollButtons(drv_buttons)
     go elevio.PollFloorSensor(drv_floors)
@@ -32,48 +33,50 @@ func main(){
 	fmt.print(a)
     
     for {
-		switch State{
-		case IDLE:
-			if ordersAbove(drv_floors){
-				SetMotorDirection(elevio.MD_Up)
-				direction = elevio.MD_Up
-				state = DRIVE
+		select{
+		case order := <- drv_buttons:
+			addOrder(order.Floor, order.Button)
+			switch State{
+			case IDLE:
+				if ordersAbove(drv_floors){
+					SetMotorDirection(elevio.MD_Up)
+					direction = elevio.MD_Up
+					state = DRIVE
+				}
+				if ordersBelow(drv_floors){
+					SetMotorDirection(elevio.MD_Down)
+					direction = elevio.MD_Down
+					state = DRIVE
+				}
+			case DRIVE:
+				if ordersInFloor(drv_floors){
+					SetMotorDirection(elevio.MD_Stop)
+					direction = elevio.MD_Stop
+					state = DOOR_OPEN
+				}
+			case DOOR_OPEN:
+				SetDoorOpenLamp(true)
+				wd := watchdog.NewWatchdog(time.Second*3)
+				if <-wd.getKickChannel(){
+					SetDoorOpenLamp(false)
+					wd.Stop()
+				}
+				orderDone <- true
+				if ordersAbove(drv_floors){
+					SetMotorDirection(elevio.MD_Up)
+					direction = elevio.MD_Up
+					state = DRIVE
+				}else if ordersBelow(drv_floors){
+					SetMotorDirection(elevio.MD_Down)
+					direction = elevio.MD_Down
+					state = DRIVE
+				}else{
+					state = IDLE
+				}
 			}
-			if ordersBelow(drv_floors){
-				SetMotorDirection(elevio.MD_Down)
-				direction = elevio.MD_Down
-				state = DRIVE
-			}
-		case DRIVE:
-			if ordersInFloor(drv_floors){
-				SetMotorDirection(elevio.MD_Stop)
-				direction = elevio.MD_Stop
-				state = DOOR_OPEN
-			}
-		case DOOR_OPEN:
-			SetDoorOpenLamp(true)
-			wd := watchdog.NewWatchdog(time.Second*3)
-			if <-wd.getKickChannel(){
-				SetDoorOpenLamp(false)
-				wd.Stop()
-			}
+		case a := <- orderDone
 			deleteOrder(drv_floors)
-			if ordersAbove(drv_floors){
-				SetMotorDirection(elevio.MD_Up)
-				direction = elevio.MD_Up
-				state = DRIVE
-			}else if ordersBelow(drv_floors){
-				SetMotorDirection(elevio.MD_Down)
-				direction = elevio.MD_Down
-				state = DRIVE
-			}else{
-				state = IDLE
-			}
-		if <-drv_buttons {
-			order := <- drv_buttons
-		}	
 		}
-        
-	    
-	
+	}
+		
 }
