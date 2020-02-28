c6531be31d683a337c8a928210be97a2bd0a127f
Odin <odin@severinsen.no>
2020-02-28 11:11:13 +0100

    Testing of type checking, failed :(

--- network/network_test.go
+++ network/network_test.go
@@ -14,7 +14,9 @@ import (
 
 func TestInitSender(t *testing.T) {
 	test_msg := TestMsg{}
+	test_msg2 := CostMsg{}
 	msg_ch := InitSender(test_msg)
+	InitSender(test_msg2)
 	localAddress, _ := net.ResolveUDPAddr("udp", ":"+TEST_MSG_PORT)
 	connection, err := net.ListenUDP("udp", localAddress)
 	if err != nil {
@@ -26,23 +28,25 @@ func TestInitSender(t *testing.T) {
 	go func() {
 		a := 19
 		for {
-			test_msg = TestMsg{a}
-			msg_ch.Ch() <- test_msg
+			test_msg2 := CostMsg{1.0, a}
+			msg_ch.Ch() <- test_msg2
 			time.Sleep(500 * time.Millisecond)
 			a = a + 1
 		}
 	}()
-	length, _, _ := connection.ReadFromUDP(inputBytes)
-	fmt.Println("Received packet!")
-	buffer := bytes.NewBuffer(inputBytes[:length])
-	decoder := gob.NewDecoder(buffer)
-	err = decoder.Decode(&rec_msg)
-	if err != nil {
-		log.Fatalf("decoding: %s", err)
-	}
-	if !cmp.Equal(rec_msg, test_msg) {
-		t.Errorf("rec_msg not equal to test_msg\nrec_msg: %+v\ntest_msg: %+v\n", rec_msg, test_msg)
-	} else {
-		fmt.Printf("Success!\nSent: %+v\nReceived %+v\n", test_msg, rec_msg)
+	for i := 0; i < 5; i++ {
+		length, _, _ := connection.ReadFromUDP(inputBytes)
+		fmt.Println("Received packet!")
+		buffer := bytes.NewBuffer(inputBytes[:length])
+		decoder := gob.NewDecoder(buffer)
+		err = decoder.Decode(&rec_msg)
+		if err != nil {
+			log.Fatalf("decoding: %s", err)
+		}
+		if !cmp.Equal(rec_msg, test_msg) {
+			t.Errorf("rec_msg not equal to test_msg\nrec_msg: %+v\ntest_msg: %+v\n", rec_msg, test_msg)
+		} else {
+			fmt.Printf("Success!\nSent: %+v\nReceived %+v\n", test_msg, rec_msg)
+		}
 	}
 }
