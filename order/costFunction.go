51b97fd54aa0d1b558d19cc039dfcea92ca19097
Odin <odin@severinsen.no>
2020-04-17 08:18:42 +0200

    init, needs to add required functions

--- order/costFunction.go
+++ order/costFunction.go
@@ -1,31 +1,29 @@
 package order
 
 import (
-	"math/rand"
-
 	"../hardware/driver-go/elevio"
 	"../network/msgs"
 )
 
-// func calculateCost(order elevio.ButtonEvent) int {
-// 	// add something for order.ButtonType == BT_Cab e.g. extremely high as it should
-// 	var cost = order.Floor - elevio.GetFloor() // Må lage getFloor, getDirection
-// 	if cost == 0 && getDirection() != MotorDirection.MD_Stop {
-// 		cost += 4
-// 	}
-// 	if cost > 0 && getDirection() == MotorDirection.MD_Down {
-// 		cost += 3
-// 	}
-// 	if cost < 0 && getDirection() == MotorDirection.MD_Up {
-// 		cost += 3
-// 	}
-// 	if cost != 0 && getDirection() == MotorDirection.MD_Stop {
-// 		cost++
-// 	}
-// 	return cost
-// }
+func calculateCost(order elevio.ButtonEvent) uint {
+	// add something for order.ButtonType == BT_Cab e.g. extremely high as it should
+	var cost = uint(order.Floor - elevio.GetFloor()) // Må lage getFloor, getDirection
+	if cost == 0 && getDirection() != elevio.MotorDirection.MD_Stop {
+		cost += 4
+	}
+	if cost > 0 && getDirection() == elevio.MotorDirection.MD_Down {
+		cost += 3
+	}
+	if cost < 0 && getDirection() == elevio.MotorDirection.MD_Up {
+		cost += 3
+	}
+	if cost != 0 && getDirection() == elevio.MotorDirection.MD_Stop {
+		cost++
+	}
+	return cost
+}
 
-func calculateCost(order elevio.ButtonEvent) uint { return uint(rand.Intn(10)) }
+// func calculateCost(order elevio.ButtonEvent) uint { return uint(rand.Intn(10)) }
 
 //getCostFuction
 
